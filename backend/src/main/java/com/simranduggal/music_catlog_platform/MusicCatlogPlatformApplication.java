eab8476538f246cc4e052227f95325a0110ec8b8
Simran Duggal <simranduggal75@gmail.com>
2026-07-29 19:46:46 +0530

    Initial project structure

---
+++ backend/src/main/java/com/simranduggal/music_catlog_platform/MusicCatlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.simranduggal.music_catlog_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCatlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCatlogPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/simranduggal/music_catlog_platform/MusicCatlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simranduggal.music_catlog_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
