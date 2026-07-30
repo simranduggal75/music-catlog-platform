e92534c83a4be277e7dc4fbfb08be1de85290521
Simran Duggal <simranduggal75@gmail.com>
2026-07-31 03:46:40 +0530

    fix: configure CORS for frontend integration

--- backend/src/main/java/com/simranduggal/music_catlog_platform/config/SecurityConfig.java
+++ backend/src/main/java/com/simranduggal/music_catlog_platform/config/SecurityConfig.java
@@ -12,6 +12,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.simranduggal.music_catlog_platform.security.JwtAuthenticationFilter;
+import java.util.List;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,7 +29,8 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+        .cors(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
@@ -45,5 +53,19 @@ public class SecurityConfig {
 AuthenticationManager authenticationManager(
         AuthenticationConfiguration configuration) throws Exception {
     return configuration.getAuthenticationManager();
+}
+@Bean
+CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
 }
 }
